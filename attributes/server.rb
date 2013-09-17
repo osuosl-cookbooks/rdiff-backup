6bcefb518d457c2a560bb18b937847f028284c5c
Sean Rettig <seanrettig@gmail.com>
2013-09-17 14:59:10 -0700

    rdiff-backup: changing default backup window

--- attributes/server.rb
+++ attributes/server.rb
@@ -1,6 +1,6 @@
 # Default attributes.
-node.default['rdiff-backup']['server']['starthour'] = 13 # (9pm PST) 
-node.default['rdiff-backup']['server']['endhour'] = 23 # (7am PST) 
+node.default['rdiff-backup']['server']['starthour'] = 5 # (9pm PST) 
+node.default['rdiff-backup']['server']['endhour'] = 13 # (5am PST) 
 node.default['rdiff-backup']['server']['user'] = "rdiff-backup-server"
 node.default['rdiff-backup']['server']['restrict-to-own-environment'] = true
 node.default['rdiff-backup']['server']['nagios-alerts'] = true
