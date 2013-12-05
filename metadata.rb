de048d6629fdd026f97a5f5d7b1831b54c41ef31
Sean Rettig <seanrettig@gmail.com>
2013-12-05 13:09:14 -0800

    Prevent nagios check from alerting if first backup is above size diff threshold

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'systems@osuosl.org'
 license          'Apache 2.0'
 description      'Installs/Configures rdiff-backup'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.3'
+version          '0.1.4'
 
 depends "sudo"
 depends "user"
