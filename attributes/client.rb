dd1add2f1d825caf8719155f343f943beeda7179
Sean Rettig <seanrettig@gmail.com>
2014-12-18 13:29:17 -0800

    update attributes files with new attributes

--- attributes/client.rb
+++ attributes/client.rb
@@ -1,5 +1,7 @@
-# Default attributes.
+# Default attributes
 node.default['rdiff-backup']['client']['ssh-port'] = "22"
 node.default['rdiff-backup']['client']['user'] = "rdiff-backup-client"
-node.default['rdiff-backup']['client']['sudo'] = true
+node.default['rdiff-backup']['client']['sudo'] = True
 node.default['rdiff-backup']['client']['jobs'] = {}
+node.default['rdiff-backup']['client']['mysql-jobs'] = {}
+node.default['rdiff-backup']['client']['mysql-all'] = True
--- attributes/server.rb
+++ attributes/server.rb
@@ -1,4 +1,6 @@
-# Default attributes.
+# Default attributes
+
+# Server attributes
 node.default['rdiff-backup']['server']['start-hour'] = 5 # (9pm PST)
 node.default['rdiff-backup']['server']['end-hour'] = 13 # (5am PST)
 node.default['rdiff-backup']['server']['user'] = "rdiff-backup-server"
@@ -8,14 +10,27 @@ node.default['rdiff-backup']['server']['restrict-to-environments'] = []
 node.default['rdiff-backup']['server']['mailto'] = ""
 node.default['rdiff-backup']['server']['nagios']['alerts'] = true
 node.default['rdiff-backup']['server']['nagios']['plugin-dir'] = "/usr/lib64/nagios/plugins"
-node.default['rdiff-backup']['server']['jobs']['default']['destination-dir'] = "/data/rdiff-backup"
-node.default['rdiff-backup']['server']['jobs']['default']['exclude-dirs'] = []
-node.default['rdiff-backup']['server']['jobs']['default']['retention-period'] = "3M"
-node.default['rdiff-backup']['server']['jobs']['default']['additional-args'] = ""
-node.default['rdiff-backup']['server']['jobs']['default']['nagios']['alerts'] = true
-node.default['rdiff-backup']['server']['jobs']['default']['nagios']['max-change'] = 8192
-node.default['rdiff-backup']['server']['jobs']['default']['nagios']['max-late-start'] = 24
-node.default['rdiff-backup']['server']['jobs']['default']['nagios']['max-late-finish-warning'] = 4
-node.default['rdiff-backup']['server']['jobs']['default']['nagios']['max-late-finish-critical'] = 8
+
+# Job attributes (filesystem)
+node.default['rdiff-backup']['server']['jobs']['']['destination-dir'] = "/data/rdiff-backup"
+node.default['rdiff-backup']['server']['jobs']['']['retention-period'] = "3M"
+node.default['rdiff-backup']['server']['jobs']['']['additional-args'] = ""
+node.default['rdiff-backup']['server']['jobs']['']['exclude-dirs'] = []
+node.default['rdiff-backup']['server']['jobs']['']['nagios']['alerts'] = true
+node.default['rdiff-backup']['server']['jobs']['']['nagios']['max-change'] = 8192
+node.default['rdiff-backup']['server']['jobs']['']['nagios']['max-late-start'] = 24
+node.default['rdiff-backup']['server']['jobs']['']['nagios']['max-late-finish-warning'] = 4
+node.default['rdiff-backup']['server']['jobs']['']['nagios']['max-late-finish-critical'] = 8
+
+# Job attributes (MySQL)
+node.default['rdiff-backup']['server']['mysql-jobs']['']['destination-dir'] = "/data/rdiff-backup"
+node.default['rdiff-backup']['server']['mysql-jobs']['']['retention-period'] = "3M"
+node.default['rdiff-backup']['server']['mysql-jobs']['']['additional-args'] = ""
+node.default['rdiff-backup']['server']['mysql-jobs']['']['single-transaction'] = ""
+node.default['rdiff-backup']['server']['mysql-jobs']['']['nagios']['alerts'] = true
+node.default['rdiff-backup']['server']['mysql-jobs']['']['nagios']['max-change'] = 8192
+node.default['rdiff-backup']['server']['mysql-jobs']['']['nagios']['max-late-start'] = 24
+node.default['rdiff-backup']['server']['mysql-jobs']['']['nagios']['max-late-finish-warning'] = 4
+node.default['rdiff-backup']['server']['mysql-jobs']['']['nagios']['max-late-finish-critical'] = 8
 
 node.force_override['authorization']['sudo']['include_sudoers_d'] = true
