2592bca1762b5c894cccd26c552ccf58def9e0cc
Sean Rettig <seanrettig@gmail.com>
2015-01-06 15:28:30 -0800

    bump version from 1.0.8 to 1.0.9

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'systems@osuosl.org'
 license          'Apache 2.0'
 description      'Installs/Configures rdiff-backup'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.8'
+version          '1.0.9'
 
 depends 'yum'
 depends 'yum-epel'
