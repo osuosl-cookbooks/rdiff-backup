1a22c34284e41c04e3a34457f84f60d4dc42c1cf
Sean Rettig <seanrettig@gmail.com>
2015-05-11 13:44:16 -0700

    add more server tests

--- test/integration/server/serverspec/server_spec.rb
+++ test/integration/server/serverspec/server_spec.rb
@@ -8,4 +8,14 @@ end
 
 describe user('rdiff-backup-server') do
   it { should exist }
+  it { should have_home_directory '/home/rdiff-backup-server' }
+end
+
+describe file('/var/log/rdiff-backup') do
+  it { should be_directory }
+  it { should be_writable.by_user('rdiff-backup-server') }
+end
+
+describe file('/etc/cron.d/rdiff-backup') do
+  it { should be_file }
 end
