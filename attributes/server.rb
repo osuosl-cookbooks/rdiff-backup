def664ee043bafdcaca1f13cfd4e0bfce2e0a80b
Jordan Evans <jordane@osuosl.org>
2015-07-27 16:28:40 -0700

    make attributes more readable, remove unused ones
    All [jobs][default] attributes are now unused

--- attributes/server.rb
+++ attributes/server.rb
@@ -6,25 +6,9 @@ default['rdiff-backup']['server']['sudo'] = true
 default['rdiff-backup']['server']['restrict-to-own-environment'] = true
 default['rdiff-backup']['server']['restrict-to-environments'] = []
 default['rdiff-backup']['server']['mailto'] = ''
-default['rdiff-backup']['server']['nagios']['alerts'] = true
-default['rdiff-backup']['server'][
-  'nagios']['plugin-dir'] = '/usr/lib64/nagios/plugins'
-default['rdiff-backup']['server'][
-  'jobs']['default']['destination-dir'] = '/data/rdiff-backup'
-default['rdiff-backup']['server']['jobs']['default']['exclude-dirs'] = []
-default['rdiff-backup']['server'][
-  'jobs']['default']['retention-period'] = '3M'
-default['rdiff-backup']['server'][
-  'jobs']['default']['additional-args'] = ''
-default['rdiff-backup']['server'][
-  'jobs']['default']['nagios']['alerts'] = true
-default['rdiff-backup']['server'][
-  'jobs']['default']['nagios']['max-change'] = 8192
-default['rdiff-backup']['server'][
-  'jobs']['default']['nagios']['max-late-start'] = 24
-default['rdiff-backup']['server'][
-  'jobs']['default']['nagios']['max-late-finish-warning'] = 4
-default['rdiff-backup']['server'][
-  'jobs']['default']['nagios']['max-late-finish-critical'] = 8
+default['rdiff-backup']['server']['nagios'] = {
+  'alerts' => true,
+  'plugin-dir' => '/usr/lib64/nagios/plugins'
+}
 
 override['authorization']['sudo']['include_sudoers_d'] = true
