c838185a0d53bb9593dc4dfd4ca593bc91dcb1e5
Jordan Evans <jordane@osuosl.org>
2015-09-11 10:13:07 -0700

    use dest for nrpe check

--- libraries/rdiff_backup.rb
+++ libraries/rdiff_backup.rb
@@ -85,18 +85,13 @@ class Chef
             source 'nagios/plugins/check_rdiff'
             cookbook 'rdiff-backup'
           end
-          # dir of repo to check
-          repo = ::File.join(new_resource.destination,
-                             'filesystem',
-                             new_resource.fqdn,
-                             new_resource.source)
           nrpe_check "check_rdiff_job_#{new_resource.name}" do
             command '/usr/bin/sudo ' +
                     ::File.join(node['nrpe']['plugin_dir'],
                                 'check_rdiff '
                                ) + "-w #{new_resource.nrpe_warning} "\
                                    "-c #{new_resource.nrpe_critical} "\
-                                   "-r #{repo} "\
+                                   "-r #{new_resource.destination} "\
                                    "-p #{new_resource.nrpe_period} "\
                                    "-l #{new_resource.nrpe_transferred}"
           end
