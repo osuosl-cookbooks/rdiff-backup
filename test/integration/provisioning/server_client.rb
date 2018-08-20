88d61f7dcf260b53099196758a1fd0fc8f144710
Jason Chen <jasonc@osuosl.org>
2018-08-20 15:13:17 -0700

    Update and fix Vagrant provisioning

--- test/integration/provisioning/server_client.rb
+++ test/integration/provisioning/server_client.rb
@@ -4,14 +4,14 @@ require 'chef/provisioning/vagrant_driver'
 with_driver "vagrant:#{File.dirname(__FILE__)}/../../../vms"
 
 machine_batch do
-  [%w(client 11), %w(server 12)].each do |name, ip_suff|
-    machine name do
+  [%w(client 11), %w(create_server 12)].each do |name, ip_suff|
+    # (host)name can't have underscores
+    machine name.tr('_', '-') do
       machine_options vagrant_options: {
-        'vm.box' => 'bento/centos-7.2',
-        'vm.box_version' => '2.2.9',
+        'vm.box' => 'bento/centos-7.5',
       },
                       convergence_options: {
-                        chef_version: '12.10.24',
+                        chef_version: '13.8.5',
                       }
       add_machine_options vagrant_config: <<-EOF
     config.vm.network "private_network", ip: "192.168.60.#{ip_suff}"
