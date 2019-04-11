d82fb1f7fc029208edcf54809e488911a27fd62a
David Gasper <dmgasper@green.workstation.cass.oregonstate.edu>
2019-04-11 13:11:29 -0700

    Update ChefSpec platform versions

--- Berksfile
+++ Berksfile
@@ -1,4 +1,5 @@
 source 'https://supermarket.chef.io'
 cookbook 'rdiff-backup-test',
          path: 'test/integration/cookbooks/rdiff-backup-test'
+cookbook 'apache2', '< 6.0.0'
 metadata
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,12 +6,12 @@ ChefSpec::Coverage.start! { add_filter 'rdiff-backup' }
 
 CENTOS_7_OPTS = {
   platform: 'centos',
-  version: '7.2.1511',
+  version: '7.4.1708',
   log_level: :fatal,
 }.freeze
 
 CENTOS_6_OPTS = {
   platform: 'centos',
-  version: '6.7',
+  version: '6.9',
   log_level: :fatal,
 }.freeze
