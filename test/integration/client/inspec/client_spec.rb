f42288a9ead37e964be2e877b9e1194dffa48a73
Khuong Luu <6059347+khashf@users.noreply.github.com>
2019-05-28 11:53:39 -0700

    Migrate from ServerSpec to InSpec (#54)
    * Basic migrations
    * Replace matchers and cron resource to their InSpec counterpart
    * Replace deprecated `have_login_shell` test
    * Long live InSpec!

--- test/integration/client/inspec/client_spec.rb
+++ test/integration/client/inspec/client_spec.rb
@@ -1,12 +1,8 @@
-require 'serverspec'
-
-set :backend, :exec
-
 describe package('rdiff-backup') do
   it { should be_installed }
 end
 
 describe user('rdiff-backup-client') do
   it { should exist }
-  it { should have_login_shell('/bin/bash') }
+  its('shell') { should eq '/bin/bash' }
 end
--- test/integration/client/inspec/localhost/rdiff_backup_spec.rb
+++ test/integration/client/inspec/localhost/rdiff_backup_spec.rb
@@ -1,7 +1,3 @@
-require 'serverspec'
-
-set :backend, :exec
-
 describe package('rdiff-backup') do
   it { should be_installed }
 end
