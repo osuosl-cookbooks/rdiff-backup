3420461a9a76a323d80a9dd0bde4adad75aa9329
Ryan <kennedy@osuosl.org>
2020-07-23 15:44:13 -0700

    Centos-8-terraform

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "centos_atomic_image" {
     default = "CentOS Atomic 7.1902"
 }
 variable "centos_image" {
-    default = "CentOS 8.1"
+    default = "CentOS 7.7"
 }
 variable "ssh_key_name" {
     default = "bootstrap"
