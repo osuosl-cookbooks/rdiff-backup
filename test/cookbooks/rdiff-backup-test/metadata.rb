455789e214599089e393ed363fb6a320d8cc2aaa
Ryan Kennedy <kennerya@oregonstate.edu>
2021-09-24 13:07:38 -0700

    Replace base with osl-resources

--- test/cookbooks/rdiff-backup-test/metadata.rb
+++ test/cookbooks/rdiff-backup-test/metadata.rb
@@ -5,5 +5,5 @@ license          'Apache-2.0'
 description      'For internal rdiff-backup testing by the rdiff-backup cookbook'
 version          '0.0.1'
 
-depends 'base'
+depends 'osl-resources'
 depends 'rdiff-backup'
