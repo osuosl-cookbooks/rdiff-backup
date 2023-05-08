8156784ba1eeec65ba246be90d6f66364b7ee0db
oweng <oweng@scarlet.workstation.cass.oregonstate.edu>
2023-05-08 12:18:01 -0700

    Remove CentOS 8

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,11 +7,6 @@ ALMA_8 = {
   version: '8',
 }.freeze
 
-CENTOS_8 = {
-  platform: 'centos',
-  version: '8',
-}.freeze
-
 CENTOS_7 = {
   platform: 'centos',
   version: '7',
@@ -20,7 +15,6 @@ CENTOS_7 = {
 ALL_PLATFORMS = [
   ALMA_8,
   CENTOS_7,
-  CENTOS_8,
 ].freeze
 
 RSpec.configure do |config|
