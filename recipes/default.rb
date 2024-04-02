eba4e124874d10e9129feab5537312c784592f9e
Zachary Wallace <zwall@osuosl.org>
2024-04-02 14:01:50 -0700

    Refactored kitchen

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,7 @@
 # Cookbook:: rdiff-backup
 # Recipe:: default
 #
-# Copyright:: 2013-2023, Oregon State University
+# Copyright:: 2013-2024, Oregon State University
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
